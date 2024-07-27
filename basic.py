29d03862813156790c8d33f872fc3c05260410c2
robotsnh <88983536+meowpu@users.noreply.github.com>
2024-07-27 10:02:33 -0700

    Update basic.py

--- basic.py
+++ basic.py
@@ -1,3 +1,8 @@
+########################################
+#CONSTANTS
+########################################
+
+
 #########################################
 #TOKENS
 #########################################
@@ -14,7 +19,7 @@ TT_RPAREN = 'RPAREN'
 class Token:
     def __init__(self,type_,values):
         self.type = type_
-        self.value=value
+        self.value=values
 
     def __repr__(self):
         if(self.value): return f'{self.type}:{self.value}'
@@ -30,7 +35,36 @@ class Lexer:
         self.text=text
         self.pos = -1
         self.current_char = None
+        self.advance()
 
     def advance(self):
         self.pos+=1
-        self.current_char = self.text[pos]
+        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+
+    def make_tokens(self):
+        tokens=[]
+        while self.current_char != None:
+            if(self.current_char in ' \t'):
+                self.advance()
+            elif self.current_char == '+':
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == '-':
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == '*':
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == '/':
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == '(':
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ')':
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            
+        
+
+        return(tokens)
